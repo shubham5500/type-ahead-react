ec58c4613b0496617a598626c4aeac32c1467412
Shubham Yadav <shubham.sy570@gmail.com>
2023-10-31 01:40:37 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 export function useDebounce(value, time) {
   const [debouncedValue, setDebouncedValue] = useState("");
   useEffect(() => {
-    console.log("CHALA", value);
 
     let timeoutId = setTimeout(() => {
       setDebouncedValue(value);
